b992e8ec98d88b62682ac6f13e1d3c20ce7d9630
Pablo <73782244+pablogiaccaglia@users.noreply.github.com>
2021-05-23 14:06:21 +0200

    first error.html commit with css config file "error_style.css"

--- code/PURE-HTML/src/it/polimi/tiw/utils/ForwardHandler.java
+++ code/PURE-HTML/src/it/polimi/tiw/utils/ForwardHandler.java
@@ -2,12 +2,14 @@ package it.polimi.tiw.utils;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
+
 
 public final class ForwardHandler {
 	
@@ -20,7 +22,8 @@ public final class ForwardHandler {
 	
 	public static void forward(HttpServletRequest request, HttpServletResponse response, String path, TemplateEngine templateEngine) throws ServletException, IOException{
 		
-		WebContext ctx = new WebContext(request, response, request.getServletContext(), request.getLocale());
+		ServletContext servletContext = request.getServletContext();
+		WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
 		templateEngine.process(path, ctx, response.getWriter());
 		
 	}
