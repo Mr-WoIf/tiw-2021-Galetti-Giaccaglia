3df60302073d5995a99d4defb44037c7d017e9f9
Mr-WoIf <filippo.galetti@mail.polimi.it>
2021-05-27 22:21:12 +0200

    Fixed some issue with the Exam Page and added link to details in the tables

--- code/PURE-HTML/src/it/polimi/tiw/dao/ExamDAO.java
+++ code/PURE-HTML/src/it/polimi/tiw/dao/ExamDAO.java
@@ -12,151 +12,148 @@ import java.util.Optional;
 import it.polimi.tiw.beans.*;
 
 public class ExamDAO {
-	
+
 	private Connection connection;
 
 	public ExamDAO(Connection connection) {
 		this.connection = connection;
 	}
-	
-	 public List<Exam> getExamsByCourseId(int courseId) throws SQLException{
-		 
-		 
-			String performedAction = " finding exams by course id";
-			
-			String query = "SELECT date, id  FROM unidb.exam WHERE course_id = ? ORDER BY date DESC";
-			
-			PreparedStatement preparedStatement = null;
-			ResultSet resultSet = null;
-			
-			List<Exam> exams = new ArrayList<>();
-			
+
+	public List<Exam> getExamsByCourseId(int courseId) throws SQLException {
+
+		String performedAction = " finding exams by course id";
+
+		String query = "SELECT date, id  FROM unidb.exam WHERE course_id = ? ORDER BY date DESC";
+
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		List<Exam> exams = new ArrayList<>();
+
+		try {
+
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, courseId);
+
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+
+				Exam exam = new Exam(new Date(resultSet.getDate("date").getTime()), resultSet.getInt("id"), courseId);
+				exams.add(exam);
+			}
+
+		} catch (SQLException e) {
+			throw new SQLException("Error accessing the DB when" + performedAction);
+
+		} finally {
 			try {
-				
-				preparedStatement = connection.prepareStatement(query);
-				preparedStatement.setInt(1, courseId);
-				
-				resultSet = preparedStatement.executeQuery();
-				
-				while(resultSet.next()) {
-			
-					Exam exam = new Exam(new Date(resultSet.getDate("date").getTime()), resultSet.getInt("id"), courseId);
-					exams.add(exam);
-				}
-				
-				
-			} catch(SQLException e) {
-				throw new SQLException("Error accessing the DB when" + performedAction);
-				
-			} finally {
-				try {
-					resultSet.close();
-				}catch (Exception e) {
-					throw new SQLException("Error closing the result set when" + performedAction);
-				}
-				try {
-					preparedStatement.close();
-				}catch (Exception e) {
-					throw new SQLException("Error closing the statement when" + performedAction);
-				}
+				resultSet.close();
+			} catch (Exception e) {
+				throw new SQLException("Error closing the result set when" + performedAction);
 			}
-			
-			
-			return exams;
-		 
-	 }
-	 
-	 public List<Exam> getSubscribedExamsByStudentID(int studentId, int courseId) throws SQLException {
-		 
-			String performedAction = " error while determining student subscription to exam";
-			
-			String query = "SELECT * FROM unidb.exam_register WHERE course_id = ? AND student_id = ?";
-			
-			PreparedStatement preparedStatement = null;
-			ResultSet resultSet = null;
-			List<Exam> exams = new ArrayList<>();
-			
-	try 	{
-				
-				preparedStatement = connection.prepareStatement(query);
-				preparedStatement.setInt(1, courseId);
-				preparedStatement.setInt(2, studentId);
-				
-				resultSet = preparedStatement.executeQuery();
-				
-				while(resultSet.next()) {
-			
-					Exam exam = new Exam(new Date(resultSet.getDate("name").getTime()), resultSet.getInt("id"), resultSet.getInt("course_id"));
-					exams.add(exam);
-				}
-				
-				
-			} catch(SQLException e) {
-				throw new SQLException("Error accessing the DB when" + performedAction);
-				
-			} finally {
-				try {
-					resultSet.close();
-				}catch (SQLException e) {
-					throw new SQLException("Error closing the result set when" + performedAction);
-				}
-				try {
-					preparedStatement.close();
-				}catch (SQLException e) {
-					throw new SQLException("Error closing the statement when" + performedAction);
-				}
+			try {
+				preparedStatement.close();
+			} catch (Exception e) {
+				throw new SQLException("Error closing the statement when" + performedAction);
 			}
-	
-	
+		}
+
 		return exams;
-			
-			
-	 }
-	 
-	 public Optional<Exam> getExamById(int examId) throws SQLException {
-		 
-		 String performedAction = " finding exam by id";
-			
-			String query = "SELECT date, course_id  FROM unidb.exam WHERE id = ?";
-			
-			PreparedStatement preparedStatement = null;
-			ResultSet resultSet = null;
-			
-			Exam exam = null;
-			
-			try {
-				
-				preparedStatement = connection.prepareStatement(query);
-				preparedStatement.setInt(1, examId);
-				
-				resultSet = preparedStatement.executeQuery();
-				
-				if(!resultSet.isBeforeFirst())
-					return Optional.ofNullable(exam);
-				
-				resultSet.next();
-				exam = new Exam(new Date(resultSet.getDate("date").getTime()), examId, resultSet.getInt("course_id"));
-				
-			} catch(SQLException e) {
-				throw new SQLException("Error accessing the DB when" + performedAction);
-				
-			} finally {
-				try {
-					resultSet.close();
-				}catch (SQLException e) {
-					throw new SQLException("Error closing the result set when" + performedAction);
-				}
-				try {
-					preparedStatement.close();
-				}catch (SQLException e) {
-					throw new SQLException("Error closing the statement when" + performedAction);
-				}
+
+	}
+
+	public List<Exam> getSubscribedExamsByStudentID(int studentId, int courseId) throws SQLException {
+
+		String performedAction = " error while determining student subscription to exam";
+
+		String query = "SELECT unidb.exam_register.student_id, unidb.exam_register.exam_id, unidb.exam.course_id, unidb.exam.id, unidb.exam.date " 
+				+ "FROM unidb.exam_register JOIN unidb.exam "
+				+ "ON exam_register.exam_id = exam.id "
+				+ "WHERE course_id = ? AND student_id = ? " 
+				+ "ORDER BY exam.date DESC ";
+
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		List<Exam> exams = new ArrayList<>();
+
+		try {
+
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, courseId);
+			preparedStatement.setInt(2, studentId);
+
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+
+				Exam exam = new Exam(new Date(resultSet.getDate("date").getTime()), resultSet.getInt("id"),
+						resultSet.getInt("course_id"));
+				exams.add(exam);
 			}
-			
-			return Optional.ofNullable(exam);
-		 
-		 
-	 }
-	 
+
+		} catch (SQLException e) {
+			throw new SQLException("Error accessing the DB when" + performedAction);
+
+		} finally {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				throw new SQLException("Error closing the result set when" + performedAction);
+			}
+			try {
+				preparedStatement.close();
+			} catch (SQLException e) {
+				throw new SQLException("Error closing the statement when" + performedAction);
+			}
+		}
+
+		return exams;
+
+	}
+
+	public Optional<Exam> getExamById(int examId) throws SQLException {
+
+		String performedAction = " finding exam by id";
+
+		String query = "SELECT date, course_id  FROM unidb.exam WHERE id = ?";
+
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		Exam exam = null;
+
+		try {
+
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, examId);
+
+			resultSet = preparedStatement.executeQuery();
+
+			if (!resultSet.isBeforeFirst())
+				return Optional.ofNullable(exam);
+
+			resultSet.next();
+			exam = new Exam(new Date(resultSet.getDate("date").getTime()), examId, resultSet.getInt("course_id"));
+
+		} catch (SQLException e) {
+			throw new SQLException("Error accessing the DB when" + performedAction);
+
+		} finally {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				throw new SQLException("Error closing the result set when" + performedAction);
+			}
+			try {
+				preparedStatement.close();
+			} catch (SQLException e) {
+				throw new SQLException("Error closing the statement when" + performedAction);
+			}
+		}
+
+		return Optional.ofNullable(exam);
+
+	}
 
 }
